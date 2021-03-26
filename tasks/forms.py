0eae8323af86c2f5ccc17dcf4265b2e4f2fda3e5
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-03-26 19:47:52 +0100

    feature(tp): implement validation for future data and time

--- tasks/forms.py
+++ tasks/forms.py
@@ -17,3 +17,27 @@ def capitalize_value(value):
         value[0].capitalize()
     return value
 
+
+class FutureDateField(DateField):
+
+    def validate(self, value):
+        super().validate(value)
+        if value <= date.today():
+            raise ValidationError('Only future dates allowed here.')
+
+    def clean(self, value):
+        result = super().clean(value)
+        return date(year=result.year, month=result.month, day=1)
+
+
+class FutureTimeField(TimeField):
+
+    def validate(self, value):
+        super().validate(value)
+        if value <= time():
+            raise ValidationError('Only future time allowed here.')
+
+    def clean(self, value):
+        result = super().clean(value)
+        return time(hour=result.hour, minute=result.minute, second=result.second)
+
