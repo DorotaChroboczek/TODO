9fc1c024a4161b28ad1acef0b9f82cd99ae2065e
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-03-26 19:02:23 +0100

    feature(tp): implement Profile model & register

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import CASCADE, Model, OneToOneField
 
-# Create your models here.
+
+class Profile(Model):
+    user = OneToOneField(User, on_delete=CASCADE)
