d2734253542a2572aff8db0d9e76da8b2035692f
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-03-28 23:19:33 +0200

    feature_change: implement rest api

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import index
+from .views import *
 
 
 urlpatterns = [
     path('', index, name='tasks'),
+    path('task-list/', task_list, name='task_list'),
+    path('task-detail/<str:pk>/', task_detail, name='task_detail'),
+    path('task-create/', task_create, name='task_create'),
+    path('task-update/<str:pk>/', task_update, name='task_update'),
+    path('task-delete/<str:pk>/', task_delete, name='task_delete'),
 ]
