c2a02b0bc555f938e595793205f603c14f945b77
DorotaChroboczek <68025504+DorotaChroboczek@users.noreply.github.com>
2021-04-12 00:51:18 +0200

    fix: resolving problems, part 1

--- TaskPlanner/settings.py
+++ TaskPlanner/settings.py
@@ -40,13 +40,13 @@ INSTALLED_APPS = [
     'accounts',
     'tasks',
     'rest_framework',
-    'corsheaders',
+    # 'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+#
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+# ]
